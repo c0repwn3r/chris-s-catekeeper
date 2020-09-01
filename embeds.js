e4a0500ac2976642bf274f84818e082a60188693
coredoescode <36366790+coredoescode@users.noreply.github.com>
2020-09-01 15:47:56 -0400

    Update embeds.js

--- embeds.js
+++ embeds.js
@@ -46,6 +46,19 @@ function playerModifed(user, rank, skipsused, skipsleft, skipsadded) {
     .addField("Won Giveaways:", giveaways, true);
     return embed;
 }
+const helpEmbed = new Discord.MessageEmbed()
+.setTitle("Help")
+.setDescription("You need to have the **Co-Owner** role or higher to run these commands.\nIf a player is not in the database and you try to preform an action on them,\nthey will be added with the default info:\nRole: Guest\nSkips: 3\nDiscord: None\nGiveaways: None)
+.setFooter("Written by core#8531 and GhostlyCoding#4665")
+.addField(".help",,true)
+.addField(".log <player username>","Retrieves information about a player from a database.",true)
+.addField(".skip <player>","Attempts to use a skip for a player. If they do not have enough skips, the command will return an error. If they do, they will have one skip removed and this action will be logged.",true)
+.addField(".player <player> skips add <amount>","Adds the amount specified to the player's Available Skips count.",true)
+.addField(".player <player> skips remove <amount>","Removes the amount specified from the player's Available Skips count.",true)
+.addField(".player <player> skips set <amount>","Sets the player's Avalible Skips count to the specified number.",true)
+.addField(".player <player> rank set <rank>","Updates a player's rank in the database and automatically updates their role in the Discord.",true)
+.addField(".player <player> discord set <disord ID with tag>","Updates a player's discord account information.",true);
+exports.helpEmbed = helpEmbed;
 exports.playerModified = playerModified;
 exports.skipFailed = failedSkip;
 exports.skipSuccess = skipSuccess;
