a4244b880d1d4e6d31c315e4389db65d59f54d66
Tyler <tylerdonia115@gmail.com>
2020-09-01 14:27:56 -0400

    Bring life

---
+++ start.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("message", msg => {
+    if (msg.content === ".hello") {
+        msg.reply("Hello! I'm Chris's Gatekeeper. Type .help for help!");
+    }
+    if (msg.content === ".help") {
+        msg.reply("Not coded yet lol");
+    }
+});
+
+client.on("ready", () => {
+    console.log("Logged in");
+});
+
+client.login("NzUwNDE4NDgzMzcxNzA0NDAx.X06PrA.WkHCy1jbRKhMUDeYCiWixa3ElKg");
