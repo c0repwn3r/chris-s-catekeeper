27aff7358da8364c3250f2a831d68da836d58dd3
Ghostly-Coding <pokeball.181818@gmail.com>
2020-09-09 19:29:51 -0500

    Added permissions

--- cx5.js
+++ cx5.js
@@ -30,40 +30,48 @@ function playerCommandParser(tokens, msg){
 function parseCommand(msg) {
     var tokens = msg.content.split(" ");
     console.log(msg.author.id);
-    switch (tokens[0]) {
-        case ".help":
-            c.emit("help", tokens, msg);
-            break;
-        case ".log":
-            c.emit("log", tokens, msg);
-            break;
-        case ".skip":
-            c.emit("skip", tokens, msg);
-            break;
-        case ".hello":
-            c.emit("hello", tokens, msg);
-            break;
-        case ".player":
-			try {
-				fs.readFileSync("./database/" + tokens[1] + ".json");
-				playerCommandParser(tokens, msg);
-			} catch (e) {
-				msg.reply("Provide a valid player.");
-			}
-            break;
-        case ".reset":
-            c.emit("reset", tokens, msg);
-            break;
-        case ".confirmdel":
-            c.emit("del", tokens, msg);
-            break;
-        case ".write":
-            c.emit("write", tokens, msg);
-            break;
-        case ".dgar":
-            c.emit("dgar", tokens, msg);
-            break;
-    }
+	if(msg.member){
+		var userRole = msg.member.roles.cache.get('721818071215374396');
+	}
+	if(!userRole) userRole = msg.member.roles.cache.get('721818097031315566');
+	if(!userRole) return;
+	console.log(userRole.name);
+	if(userRole.id === '721818071215374396' || userRole.id === '721818097031315566') {
+		switch (tokens[0]) {
+			case ".help":
+				c.emit("help", tokens, msg);
+				break;
+			case ".log":
+				c.emit("log", tokens, msg);
+				break;
+			case ".skip":
+				c.emit("skip", tokens, msg);
+				break;
+			case ".hello":
+				c.emit("hello", tokens, msg);
+				break;
+			case ".player":
+				try {
+					fs.readFileSync("./database/" + tokens[1] + ".json");
+					playerCommandParser(tokens, msg);
+				} catch (e) {
+					msg.reply("Provide a valid player.");
+				}
+				break;
+			case ".reset":
+				c.emit("reset", tokens, msg);
+				break;
+			case ".confirmdel":
+				c.emit("del", tokens, msg);
+				break;
+			case ".write":
+				c.emit("write", tokens, msg);
+				break;
+			case ".dgar":
+				c.emit("dgar", tokens, msg);
+				break;
+		}
+	}
 }
 exports.c = c;
 exports.parseCommand = parseCommand;
